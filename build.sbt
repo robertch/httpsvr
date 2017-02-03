0e440e8398e5f657120b1a3e03d57c5b39a3fa65
Robert Chojnacki <rc@heuthes.pl>
2017-02-03 13:43:01 +0100

    not working start

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "httpsvr"
+mainClass in (Compile,run) := Some("httpsvr.Server")
+version := "1.0"
+
+scalaVersion := "2.11.2"
---
+++ src/main/scala/httpsvr/Server.scala
@@ -0,0 +1,44 @@
+package httpsvr
+
+import java.util.Map
+import java.util.logging.Logger
+
+import org.nanohttpd.protocols.http._
+import org.nanohttpd.protocols.http.request.Method
+import org.nanohttpd.protocols.http.response.Response
+import org.nanohttpd.util.ServerRunner
+object Server{
+  
+    def main(args : Array[String]) {
+        ServerRunner.run(new Server(8080).getClass)
+    }
+}
+class Server(port:Int) extends NanoHTTPD(port){
+
+  val LOG = Logger.getLogger(this.getClass.getName)
+
+
+    // def this() {
+    //     this(8080)
+    // }
+
+    override def serve(session:IHTTPSession):Response = {
+        val method:Method = session.getMethod
+        val uri:String = session.getUri
+
+        this.LOG.info(method + " '" + uri + "' ")
+
+        var msg:String = "<html><body><h1>Hello server</h1>\n"
+        val parms:Map[String,String] = session.getParms
+        if(parms.get("username") == null){
+            msg += "<form action='?' method='get'>\n" 
+            msg += "  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n"
+        } else {
+            msg += "<p>Hello, " + parms.get("username") + "!</p>"
+        }
+
+        msg += "</body></html>\n"
+
+        Response.newFixedLengthResponse(msg)
+    }
+}
